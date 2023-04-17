a0b27df7ff03c15b4721f0235c0d666ce06fa875
itaihochman <itai.hochman@gmail.com>
2023-04-17 22:38:17 +0300

    Refactor to avoid circular imports

--- autogpt/processing/text.py
+++ autogpt/processing/text.py
@@ -1,42 +1,13 @@
 """Text processing functions"""
-from typing import Generator, Optional, Dict
+from typing import Optional, Dict
 from selenium.webdriver.remote.webdriver import WebDriver
 from autogpt.memory import get_memory
 from autogpt.config import Config
 from autogpt.llm_utils import create_chat_completion
+from autogpt.processing.split import split_text
 
 CFG = Config()
 MEMORY = get_memory(CFG)
-
-
-def split_text(text: str, max_length: int = 8192) -> Generator[str, None, None]:
-    """Split text into chunks of a maximum length
-
-    Args:
-        text (str): The text to split
-        max_length (int, optional): The maximum length of each chunk. Defaults to 8192.
-
-    Yields:
-        str: The next chunk of text
-
-    Raises:
-        ValueError: If the text is longer than the maximum length
-    """
-    paragraphs = text.split("\n")
-    current_length = 0
-    current_chunk = []
-
-    for paragraph in paragraphs:
-        if current_length + len(paragraph) + 1 <= max_length:
-            current_chunk.append(paragraph)
-            current_length += len(paragraph) + 1
-        else:
-            yield "\n".join(current_chunk)
-            current_chunk = [paragraph]
-            current_length = len(paragraph) + 1
-
-    if current_chunk:
-        yield "\n".join(current_chunk)
 
 
 def summarize_text(
